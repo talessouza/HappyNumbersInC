63c7d51ab62db67b43f5d6c6b2a0d58e1db52ead
Tales Wallace Souza <tales.wallace@gmail.com>
2017-04-01 02:05:57 -0300

    criado o programa base para realizar os testes

---
+++ happynumber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio2.h>
+
+
+void test(char * esperado, char * atual, char * mensagem) {
+	int resultado_comparacao = strcmp(esperado, atual);
+	if (resultado_comparacao == 0)
+	{
+		printf("%s - TEST OK! !!!\n", mensagem);
+	} else 
+	{
+		textcolor(RED);
+		printf("%s - ****ERRO**** NO TEST - Esperado: [%s] retornado: [%s]!!!\n", mensagem, esperado, atual);
+		textcolor(LIGHTGRAY);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+	test("feliz", "feliz", "test 1");
+	test("feliz", "triste", "test 4");
+	test("feliz", "feliz", "test 100");
+	
+	return 0;
+}
